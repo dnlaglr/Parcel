23a5a060c57fd1d084153d1777bd11f7150e359f
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-09-04 08:32:15 -0700

    Create CLI client entry

---
+++ parcel/cli/cli.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+#include "commands/helpCommand.h"
+#include "commands/installCommand.h"
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) { std::cout << "Error: You must specify a command." << std::endl; }
+
+  if (std::string(argv[1]) == "help") {
+    HelpCommand cmd;
+    cmd.execute();
+  } else if (std::string(argv[1]) == "install") {
+    InstallCommand cmd;
+    
+    std::vector<std::string> args(argv + 2, argv + argc);
+
+    cmd.execute(args);
+  }
+}
